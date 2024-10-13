1e2ebcaffa3f91d346fec4da4b8d82626291123b
piseb <piseb@tuta.com>
2024-10-13 18:17:25 +0200

    feature(doc): adding documentation and API auto-documentation (#2)

--- src/apiv1/urls.py
+++ src/apiv1/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.conf import settings
 from rest_framework.routers import DefaultRouter
 
 from . import views
@@ -10,3 +11,21 @@ router.register(r"events", views.EventViewSet)
 urlpatterns = [
     path("", include(router.urls)),
 ]
+
+# if in DEBUG mode: add auto documentation from drf_spectacular
+if settings.DEBUG:
+    from drf_spectacular.views import (
+        SpectacularAPIView,
+        SpectacularRedocView,
+        SpectacularSwaggerView,
+    )
+
+    urlpatterns += [
+        path("schema/", SpectacularAPIView.as_view(), name="schema"),
+        path(
+            "swagger-ui/",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
+        ),
+        path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    ]
