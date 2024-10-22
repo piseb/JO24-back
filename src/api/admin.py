0df7e0e9b1c78e60630dfbcd2761f6feb13b8769
piseb <piseb@tuta.com>
2024-10-22 12:28:26 +0200

    feature(dev): configuring Django Admin OOB for dev

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from api import models
+
+
+class DisciplineAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+
+
+class EventAdmin(admin.ModelAdmin):
+    search_fields = ["title"]
+    list_display = ["title", "begin_at", "end_at"]
+    date_hierarchy = "begin_at"
+
+
+class OfferAdmin(admin.ModelAdmin):
+    search_fields = ["title"]
+    list_display = ["title", "price", "disable"]
+    list_filter = ["disable"]
+
+
+admin.site.register(models.Discipline, DisciplineAdmin)
+admin.site.register(models.Event, EventAdmin)
+admin.site.register(models.Offer, OfferAdmin)
